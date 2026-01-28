0766d9decb495297374628159c98069361af428d
Yang Liu <yangliu0903@gmail.com>
2026-01-28 22:43:49 +0800

    chore: fix vite config aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // 关键配置：设置为相对路径，适配 GitHub Pages 等子目录部署环境
   base: './',
+  resolve: {
+    alias: {
+      '@': process.cwd(),
+    },
+  },
   server: {
     port: 3000,
     open: true
